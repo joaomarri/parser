ee776fd4d5107c7fdd01c237ff00efe179da98a7
João Paulo Coltri Marri <joaopaulo@MacBook-Pro-de-Joao.local>
2018-02-07 23:59:10 -0200

    log file tests

--- src/main/java/com/ef/service/ReaderLogService.java
+++ src/main/java/com/ef/service/ReaderLogService.java
@@ -2,6 +2,10 @@ package com.ef.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.slf4j.Logger;
@@ -15,32 +19,55 @@ public class ReaderLogService {
 
 	private static final Logger logger = LoggerFactory.getLogger(ReaderLogService.class);
 	
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	
 	public void readLogFile(String fileName) {
 		logger.info("Reading log file {}", fileName);
 		
+		Scanner scanner = null;
+		
 		try {
-			Scanner scanner = new Scanner(new File(fileName));
-
-			while (scanner.hasNext()){
+			List<AccessLog> logs = new ArrayList<>(); 
+			scanner = new Scanner(new File(fileName));
+			int count = 0;
+			
+			while (scanner.hasNext()) {
 				String line = scanner.nextLine();
-				logger.info(line);
+				AccessLog log = mapToItem(line);
+				logs.add(log);
+				count++;
+				if (count == 100) {
+					//persist na base os 100
+					count = 0;
+					logs = new ArrayList<>(); 
+				}
 				
+				logger.info(line);
 			}
 
 		} catch (IOException e) {
 			logger.error("Error when try to read the file {}", fileName, e);
+		} catch (ParseException p) {
+			logger.error("Error when try to parse the file {}", fileName, p);
+		} catch (Exception ex) {
+			logger.error("Error in the process parse {}", fileName, ex);
+		} finally {
+			if (scanner!=null) {
+				scanner.close();
+			}
 		}
 	}
 	
-	private AccessLog mapToItem(String line) {
+	private AccessLog mapToItem(String line) throws ParseException {
 		AccessLog log = null;
 		String[] tokens = line.split("|");
 		if (tokens != null) {
 			log = new AccessLog();
-			log.setDate(tokens[0]);
+			log.setDate(dateFormat.parse(tokens[0]));
 			log.setIp(tokens[1]);
 			log.setRequest(tokens[2]);
-			log.setStatus(tokens[3]);
+			log.setStatus(Integer.valueOf(tokens[3]));
 			log.setUserAgent(tokens[4]);
 		}
 		
