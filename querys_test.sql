3b1d53f70ad6ba1e73b157194b739e07ba72607f
João Paulo Coltri Marri <jamarri@marketdata.com.br>
2018-02-09 14:54:03 -0200

    add sql script test

---
+++ querys_test.sql
@@ -0,0 +1,13 @@
+--SQL querys
+--1)
+select ip from accessLog
+where
+dateLog >= '2017-01-01 13:00:00'
+and dateLog <= '2017-01-01 14:00:00'
+Group by ip
+Having count(ip) > 2
+
+--2)
+select * from accessLog
+where
+ip = '192.168.12.4'
